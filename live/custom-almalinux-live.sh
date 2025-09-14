e136135fdaf209c32e274a463e4bf90fe73b2f54
Sergey <10003391+gnomba@users.noreply.github.com>
2025-09-14 23:44:32 +0300

    modified:   live/custom-almalinux-live.sh

--- live/custom-almalinux-live.sh
+++ live/custom-almalinux-live.sh
@@ -102,12 +102,14 @@ echo "[+] Пересобираем squashfs..."
 cd ..
 sudo mksquashfs squashfs-root ../LiveOS/squashfs.img -comp xz -b 1M -Xbcj x86 -noappend
 
+rm -rfv squashfs-root
+
 cd ..
 
 echo "[+] Собираем новый ISO..."
 xorriso -as mkisofs -o "../$CUSTOM_ISO" \
-  -isohybrid-mbr /usr/share/syslinux/isohdpfx.bin \
-  -c isolinux/boot.cat -b isolinux/isolinux.bin \
+  -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
+  -c isolinux/boot.cat -b /usr/lib/ISOLINUX/isolinux.bin \
   -no-emul-boot -boot-load-size 4 -boot-info-table \
   -eltorito-alt-boot -e images/efiboot.img -no-emul-boot \
   .
