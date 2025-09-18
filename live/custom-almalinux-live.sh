82df7327d2d3e4ec5356ad26342cc945f952b0e4
Sergey <10003391+gnomba@users.noreply.github.com>
2025-09-18 11:35:21 +0300

    Update custom-almalinux-live.sh

--- live/custom-almalinux-live.sh
+++ live/custom-almalinux-live.sh
@@ -26,7 +26,7 @@ ROOTPASS="rootpass"    # пароль для root
 PUBKEY_FILE="$HOME/.ssh/id_rsa.pub"  # ваш публичный ключ
 
 # === Подготовка ===
-mkdir -p "$WORKDIR" "$MOUNTDIR"
+mkdir -pv "$WORKDIR" "$MOUNTDIR"
 
 echo "[+] Скачиваем ISO... ("$ISO_URL" --> "$ISO_NAME")"
 [ -f "$ISO_NAME" ] || curl -L "$ISO_URL" -o "$ISO_NAME"
@@ -35,31 +35,36 @@ echo "[+] Монтируем ISO... ("$ISO_NAME" --> "$MOUNTDIR")"
 sudo mount -o loop "$ISO_NAME" "$MOUNTDIR"
 
 echo "[+] Копируем содержимое ISO... ("$MOUNTDIR/" --> "$WORKDIR/")"
-rsync -a "$MOUNTDIR/" "$WORKDIR/"
+sudo rsync -a "$MOUNTDIR/" "$WORKDIR/"
 
 sudo umount -v "$MOUNTDIR"
 
 cd "$WORKDIR"
 
 echo "[+] Распаковываем squashfs..."
-mkdir squashfs
+sudo mkdir squashfs
 cd squashfs
-unsquashfs ../LiveOS/squashfs.img
+sudo unsquashfs ../LiveOS/squashfs.img
 cd squashfs-root
 
-losetup --find --partscan --show LiveOS/rootfs.img 
-
-mkdir -pv /mnt/rootfs
-mount /dev/loop0 /mnt/rootfs
+if [[ "$VERSION" == "8" || "$VERSION" == "9" ]]; then
+    sudo losetup --find --partscan --show LiveOS/rootfs.img 
+    vLOOPDEV="$(sudo losetup -l | grep rootfs | awk '{print $1}')"
+    vROOFSDIR="/mnt/rootfs"
+    sudo mkdir -pv ${vROOFSDIR}
+    sudo mount -v ${vLOOPDEV} ${vROOFSDIR}
+else
+    vROOFSDIR="."
+fi
 
 echo "[+] Настраиваем sshd..."
 # Включаем root, пароль и ключи
-sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /mnt/rootfs/etc/ssh/sshd_config
-sudo sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication yes/' /mnt/rootfs/etc/ssh/sshd_config
-sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /mnt/rootfs/etc/ssh/sshd_config
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' ${vROOFSDIR}/etc/ssh/sshd_config
+sudo sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication yes/' ${vROOFSDIR}/etc/ssh/sshd_config
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' ${vROOFSDIR}/etc/ssh/sshd_config
 
 echo "[+] Добавляем systemd unit для SSH..."
-cat <<EOF | sudo tee /mnt/rootfs/etc/systemd/system/live-ssh.service
+cat <<EOF | sudo tee ${vROOFSDIR}/etc/systemd/system/live-ssh.service
 [Unit]
 Description=Enable SSH in Live session
 After=network.target
@@ -75,35 +80,39 @@ ExecStart=/bin/systemctl start sshd
 WantedBy=multi-user.target
 EOF
 
-sudo ln -s /etc/systemd/system/live-ssh.service /mnt/rootfs/etc/systemd/system/multi-user.target.wants/live-ssh.service
+sudo ln -s /etc/systemd/system/live-ssh.service ${vROOFSDIR}/etc/systemd/system/multi-user.target.wants/live-ssh.service
 
 # === Добавляем ключи ===
 if [ -f "$PUBKEY_FILE" ]; then
     echo "[+] Копируем публичный ключ для liveuser..."
-    sudo mkdir -p /mnt/rootfs/home/liveuser/.ssh
-    sudo cp "$PUBKEY_FILE" /mnt/rootfs/home/liveuser/.ssh/authorized_keys
-    sudo chmod 700 /mnt/rootfs/home/liveuser/.ssh
-    sudo chmod 600 /mnt/rootfs/home/liveuser/.ssh/authorized_keys
-    sudo chown -R 1000:1000 /mnt/rootfs/home/liveuser/.ssh  # UID/GID liveuser
+    sudo mkdir -pv ${vROOFSDIR}/home/liveuser/.ssh
+    sudo cp "$PUBKEY_FILE" ${vROOFSDIR}/home/liveuser/.ssh/authorized_keys
+    sudo chmod 700 ${vROOFSDIR}/home/liveuser/.ssh
+    sudo chmod 600 ${vROOFSDIR}/home/liveuser/.ssh/authorized_keys
+    sudo chown -R 1000:1000 ${vROOFSDIR}/home/liveuser/.ssh  # UID/GID liveuser
 
     echo "[+] Копируем публичный ключ для root..."
-    sudo mkdir -p /mnt/rootfs/root/.ssh
-    sudo cp "$PUBKEY_FILE" /mnt/rootfs/root/.ssh/authorized_keys
-    sudo chmod 700 /mnt/rootfs/root/.ssh
-    sudo chmod 600 /mnt/rootfs/root/.ssh/authorized_keys
-    sudo chown -R 0:0 /mnt/rootfs/root/.ssh
+    sudo mkdir -pv ${vROOFSDIR}/root/.ssh
+    sudo cp "$PUBKEY_FILE" ${vROOFSDIR}/root/.ssh/authorized_keys
+    sudo chmod 700 ${vROOFSDIR}/root/.ssh
+    sudo chmod 600 ${vROOFSDIR}/root/.ssh/authorized_keys
+    sudo chown -R 0:0 ${vROOFSDIR}/root/.ssh
 else
     echo "[-] Публичный ключ для liveuser/root отсутствует..."
 fi
 
 echo "[+] Добавляем HDSentinel..."
 wget https://www.hdsentinel.com/hdslin/hdsentinel-020c-x64.zip -O /tmp/hdsentinel-020c-x64.zip
-unzip /tmp/hdsentinel-020c-x64.zip -d /mnt/rootfs/usr/local/bin
-chmod +x /mnt/rootfs/usr/local/bin/HDSentinel
+sudo unzip /tmp/hdsentinel-020c-x64.zip -d ${vROOFSDIR}/usr/local/bin
+sudo chmod +x ${vROOFSDIR}/usr/local/bin/HDSentinel
 
 sync
-umount -fv /mnt/rootfs
-losetup -d /dev/loop0
+sudo sync
+
+if [[ "$VERSION" == "8" || "$VERSION" == "9" ]]; then
+    sudo umount -fv ${vROOFSDIR}
+    sudo losetup -d ${vLOOPDEV}
+fi
 
 echo "[+] Пересобираем squashfs..."
 cd ..
@@ -111,18 +120,26 @@ sudo mksquashfs squashfs-root ../LiveOS/squashfs.img -comp xz -b 1M -Xbcj x86 -n
 
 cd ..
 
-rm -rfv squashfs
+sudo rm -rfv squashfs
 
 echo "[+] Собираем новый ISO..."
-xorriso -as mkisofs -o "../$CUSTOM_ISO" \
+if [[ "$VERSION" == "8" || "$VERSION" == "9" ]]; then
+  sudo xorriso -as mkisofs -o "../$CUSTOM_ISO" \
   -isohybrid-mbr isolinux/isolinux.bin \
   -b isolinux/isolinux.bin \
   -no-emul-boot -boot-load-size 4 -boot-info-table \
   -eltorito-alt-boot -e images/efiboot.img -no-emul-boot \
   .
+  sudo rm -rfv ${vROOFSDIR}
+else
+  sudo xorriso -as mkisofs -o "../$CUSTOM_ISO" \
+  -no-emul-boot -boot-load-size 4 -boot-info-table \
+  -eltorito-alt-boot -e images/eltorito.img -no-emul-boot \
+  .
+fi
 
 cd ..
-rm -rfv $WORKDIR $MOUNTDIR
+sudo rm -rfv $WORKDIR $MOUNTDIR
 
 echo "[+] Готово! Новый ISO: $CUSTOM_ISO"
 echo "    Доступные пользователи:"
